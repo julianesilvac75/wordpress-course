93a44d269b06d6cb62ab2922c4b231c16c330241
Juliane <julianesilvac75@gmail.com>
2022-10-12 11:54:03 +0100

    class 8 PHP functions

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,19 @@
-This is our completely amazing custom theme!
+<?php
+  // function myFirstFunction() {
+  //   echo "<p>Hello, this is a function</p>";
+  // }
+
+  // myFirstFunction();
+  // myFirstFunction();
+  // myFirstFunction();
+
+  function greet($name, $color) {
+    echo "<p>Hi, my name is $name and my favorite color is $color.</p>";
+  }
+
+  greet('John', 'blue');
+  greet('Jane', 'green');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
